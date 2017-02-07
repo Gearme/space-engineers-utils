6eba187e2fff6caedb982cbe97048e6aea94c98f
SWindrath <steffen.windrath@haw-hamburg.de>
2017-02-08 00:21:11 +0100

    Update hangar-status.cs

--- Scripts/hangar-status.cs
+++ Scripts/hangar-status.cs
@@ -4,46 +4,54 @@
 //Example: 
 //Arguments: 1;in, 1;out
 //Light group name: *Light 1*
-static Color occupiedColor = new Color(196,48,48);  
-static Color FreeColor = new Color(50,149,45);  
+static Color occupiedColor = new Color(196,48,48); 
+static Color FreeColor = new Color(50,149,45); 
+
+bool state; 
+List<IMyInteriorLight> lights;
+IMyShipConnector connector;
  
-bool state;  
-List<IMyInteriorLight> lights; 
-  
-public Program() { 
-    //Load current State  
-    if (!bool.TryParse(Storage, out state)) { // Couldnt parse -> default Value false  
-        state = false; 
-    }  
-}  
-  
-public void Save() {  
-    //Save state variable 
-    Storage = Convert.ToString(state);    
-}  
-  
-public void Main(string argument) { 
+public Program() {
+    //Load current State 
+    if (!bool.TryParse(Storage, out state)) { // Couldnt parse -> default Value false 
+        state = false;
+    } 
+} 
+ 
+public void Save() { 
+    //Save state variable
+    Storage = Convert.ToString(state);   
+} 
+ 
+public void Main(string argument) {
     string[] arguments = argument.Split(';');
     prepareLightGroup(arguments[0]);
-    // If Lights are green (!state) and need to be changed to red (isActive()) 
-    if(!state){ 
-        setColor(occupiedColor, true);  
-    // If Lights are red and need to be changed to green 
-    }else if(state){       
-        setColor(FreeColor, false);  
-    }  
-} 
-  
-// prepare the lights  
-public void prepareLightGroup(string groupName){  
-    var lightsGroup = GridTerminalSystem.GetBlockGroupWithName("Light " + groupName);  
-    lights = new List<IMyInteriorLight>(); 
-    lightsGroup.GetBlocksOfType(lights); 
-} 
- 
-public void setColor(Color color, bool newState){ 
-    foreach (IMyInteriorLight light in lights){ 
-        light.SetValue("Color", color); 
+    prepareConnector(arguments[0]);
+    // If Lights are green (!state) and need to be changed to red (isActive())
+    if(!state){
+        setColor(occupiedColor, true); 
+    // If Lights are red and need to be changed to green
+    }else if(state){
+        if(!connector.IsLocked){ 
+            setColor(FreeColor, false);
+        }
     } 
-    state = newState; 
+}
+ 
+// prepare the lights 
+public void prepareLightGroup(string groupName){ 
+    var lightsGroup = GridTerminalSystem.GetBlockGroupWithName("Light " + groupName); 
+    lights = new List<IMyInteriorLight>();
+    lightsGroup.GetBlocksOfType(lights);
+}
+
+public void prepareConnector(string groupName){
+    connector = GridTerminalSystem.GetBlockWithName("Connector " + groupName) as IMyShipConnector; 
+}
+
+public void setColor(Color color, bool newState){
+    foreach (IMyInteriorLight light in lights){
+        light.SetValue("Color", color);
+    }
+    state = newState;
 }
